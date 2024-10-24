46771012fe1cbec64b3a6dbb201a3d3853949b9f
gromanate <romanforgit@gmail.com>
2024-10-24 08:31:01 +0545

    Updated the icon

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
 dotenv.config(); // Load environment variables from .env file
 
 import todoRouter from "./routes/todo.js";
 import connectdb from "./db/connectdb.js";
 
-const app = express();
 
 // Middleware
 app.use(express.json()); // Parse incoming JSON requests
 
 // Use the todo router for the "/api/v1/todo" path
 app.use("/api/v1/todo", todoRouter);
+
+// only when ready to deploy
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 const port = process.env.PORT || 5000; // Use PORT (uppercase)
 
