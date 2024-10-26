4f8ec3e4b85da6eea52e0ab9b3afec307397a723
gromanate <romanforgit@gmail.com>
2024-10-26 20:04:14 +0545

    update url

---
+++ models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
