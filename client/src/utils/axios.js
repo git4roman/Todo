a1bdf50bf666203bbad2105835d089e5321bd235
git4roman <romanforgit@gmail.com>
2024-11-15 09:56:37 +0545

    baseURl comma

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_URL;
 const axiosInstance = axios.create({
-  baseURL: "https://todo-demo-alpha.vercel.app",
+  baseURL: URL || "http://localhost:5000",
 });
 
 export default axiosInstance;
