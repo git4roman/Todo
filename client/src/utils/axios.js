aaf44cfad445f6a6e037cb900739790f8ff8d4df
Roman Khatri <157000687+git4roman@users.noreply.github.com>
2024-11-15 09:34:46 +0545

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_URL;
 const axiosInstance = axios.create({
-  baseURL:  "https://todo-demo-alpha.vercel.app/login",
+  baseURL:  "https://todo-demo-alpha.vercel.app",
 });
 
 export default axiosInstance;
