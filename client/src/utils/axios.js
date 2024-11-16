cbc4568b1e493dc84979bf38c929eea3662484d5
git4roman <romanforgit@gmail.com>
2024-11-16 08:26:34 +0545

    baseURl proxy

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const URL = process.env.REACT_APP_URL;
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: URL || "http://localhost:5000",
 });
 
 export default axiosInstance;
