5f815b0caca0089b87103a1ed2656e9cc6a7b779
git4roman <romanforgit@gmail.com>
2024-11-19 16:59:49 +0545

    New message

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
 const axiosInstance = axios.create({
   baseURL: `${URL}/api/v1`, // Removed unnecessary double quotes
 });
